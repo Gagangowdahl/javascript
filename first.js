1a7df57cf895553857f8a11f059c4c5ee5160d1a
GAGAN H L <137033004+Gagangowdahl@users.noreply.github.com>
2024-05-03 18:26:30 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,21 @@
+// object
+const student = {
+    fullname : "manu",
+     age :24,
+     cgp : 8.5,
+     marks : true
+};  
+student["age"] = student["age"] + 1
+
+console.log(student.age);
+const product ={
+    title:"ball pen",
+    rating:5,
+    price:25,
+    offer:5
+};
+
+// operator
+let a=10;
+let b=20;
+console.log(a+b);
---
+++ second.js
@@ -0,0 +1,17 @@
+// addition of operator
+// let a=10;
+// let b=20;
+// let c=a+b;
+// console.log(c);
+
+
+// unary operator
+let a=10;
+let b=20;
+++a;
+--b;
+console.log(a);
+console.log(b);
+
+
+
